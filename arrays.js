65b288ebb19dea631371dfdfabb89b0259f54c68
Man Hong Lee <manhong323@gmail.com>
2017-10-17 19:00:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,8 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
